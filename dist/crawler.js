9866cc53d221284c29896bf94ed08c5db8f431a9
cain <bys20411@gmail.com>
2023-07-09 11:23:34 +0900

    init

---
+++ dist/crawler.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.extractTopStockItem = void 0;
+const extractTopStockItem = () => { };
+exports.extractTopStockItem = extractTopStockItem;
